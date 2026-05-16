6d25a5d49bb504c74e22640f17eae1ef6551d0fd
alpborank <alpborankdev@gmail.com>
2026-05-16 21:24:02 +0200

    Update reset-password.js

--- reset-password.js
+++ reset-password.js
@@ -1,3 +1,5 @@
+import { createClient }
+from "https://esm.sh/@supabase/supabase-js";
 const SUPABASE_URL='https://epzgbrmlkjdqzfutskyr.supabase.co'
 const SUPABASE_ANON='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVwemdicm1sa2pkcXpmdXRza3lyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwMzAyMDgsImV4cCI6MjA5MjYwNjIwOH0.Lvzr78kN3XFv_dstfckIv5x2YPBmn52Sz2DAeN1bGII'
 
